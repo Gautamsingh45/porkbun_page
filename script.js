c56b829cdf5b350660a0d1f94cb2cfa822c4995d
Gautamsingh45 <gautamsingh893591@gmail.com>
2023-06-12 13:23:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Open login popup
+document.getElementById("login-button").addEventListener("click", function() {
+    document.getElementById("login-popup").style.display = "block";
+  });
+  
+  // Close login popup
+  document.getElementById("close-button").addEventListener("click", function() {
+    document.getElementById("login-popup").style.display = "none";
+  });
+  
